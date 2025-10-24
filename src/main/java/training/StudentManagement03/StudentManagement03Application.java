7b1888d28b7a1b85563a110519cf3eb534105dca
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-10-24 14:15:54 +0900

    スタンダード第18回

--- src/main/java/training/StudentManagement03/StudentManagement03Application.java
+++ src/main/java/training/StudentManagement03/StudentManagement03Application.java
@@ -1,13 +1,10 @@
 package training.StudentManagement03;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,29 +18,8 @@ public class StudentManagement03Application {
 		SpringApplication.run(StudentManagement03Application.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-		return "私は" + student.getName() + "です。" + " " + student.getAge() + "歳です。";
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudent(String name, int age) {
-		repository.updateStudent(name,age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 	}
 }
-
-// localhost:8080
-// HTTPメソッドの　GETとPOSt
-// GETは取得、リクエストの結果を受け取る。
-// POSTは情報を与える、渡す。
