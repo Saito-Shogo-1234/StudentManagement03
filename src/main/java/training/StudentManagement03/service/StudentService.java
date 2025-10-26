5b6ee9008b094aa226c99e722edee99067684c01
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-10-26 13:31:48 +0900

    スタンダード第20回

---
+++ src/main/java/training/StudentManagement03/service/StudentService.java
@@ -0,0 +1,40 @@
+package training.StudentManagement03.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import training.StudentManagement03.data.Student;
+import training.StudentManagement03.data.StudentCourse;
+import training.StudentManagement03.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentsList() {
+    return repository.search();
+  }
+
+  public List<StudentCourse> searchStudentsCourseList() {
+    return repository.searchCourse();
+  }
+
+  public List<Student> search30StudentsList() {
+    return repository.search().stream()
+        .filter(s -> s.getAge() >= 15 && s.getAge() < 17)
+        .collect(Collectors.toList());
+  }
+
+  public List<StudentCourse> search30StudentsCourseList() {
+    return repository.searchCourse().stream()
+        .filter(s -> s.getCourseName().equals("サッカー")||s.getCourseName().equals("野球"))
+        .collect(Collectors.toList());
+  }
+}
