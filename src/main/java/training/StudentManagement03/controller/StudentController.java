3266095b9b720ec0a99be6a49e5a4ee58f67c434
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-10-28 01:23:01 +0900

    スタンダード第21回

--- src/main/java/training/StudentManagement03/controller/StudentController.java
+++ src/main/java/training/StudentManagement03/controller/StudentController.java
@@ -4,37 +4,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import training.StudentManagement03.controller.converter.StudentConverter;
 import training.StudentManagement03.data.Student;
-import training.StudentManagement03.data.StudentCourse;
+import training.StudentManagement03.data.StudentCourses;
+import training.StudentManagement03.domain.StudentDetail;
 import training.StudentManagement03.service.StudentService;
 
 @RestController
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentsList")
-  public List<Student> getStudentsList() {
-    return service.searchStudentsList();
+  public List<StudentDetail> getStudentsList() {
+    List<Student> students = service.searchStudentsList();
+    List<StudentCourses> studentCourses = service.searchStudentsCourseList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
   @GetMapping("/studentsCourseList")
-  public List<StudentCourse> getStudentsCourseList() {
+  public List<StudentCourses> getStudentsCourseList() {
     return service.searchStudentsCourseList();
   }
 
   @GetMapping("/30studentsList")
-  public List<Student> get30StudentsList() {
-    return service.search30StudentsList();
+  public List<StudentDetail> get30StudentsList() {
+    List<Student> students = service.search30StudentsList();
+    List<StudentCourses> studentCourses = service.search30StudentsCourseList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
   @GetMapping("/30studentCourseList")
-  public List<StudentCourse> get30StudentsCourseList() {
+  public List<StudentCourses> get30StudentsCourseList() {
     return service.search30StudentsCourseList();
   }
 }
