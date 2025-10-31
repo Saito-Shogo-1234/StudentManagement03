dd035fa81c328a41f23287419319b3d7d78e0019
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-10-31 12:22:49 +0900

    スタンダード第25回

--- src/main/java/training/StudentManagement03/controller/StudentController.java
+++ src/main/java/training/StudentManagement03/controller/StudentController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import training.StudentManagement03.controller.converter.StudentConverter;
 import training.StudentManagement03.data.Student;
 import training.StudentManagement03.data.StudentCourses;
@@ -48,5 +51,20 @@ public class StudentController {
   @GetMapping("/30studentCourseList")
   public List<StudentCourses> get30StudentsCourseList() {
     return service.search30StudentsCourseList();
+  }
+
+  @GetMapping("/newStudent")
+  public  String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+    // System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+    return "redirect:/studentList";
   }
 }
