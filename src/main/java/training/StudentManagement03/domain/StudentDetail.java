9d8c5df73d6a1742d9f4826abc9d6455719ae2c5
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-11-11 14:16:25 +0900

    スタンダード第38回

--- src/main/java/training/StudentManagement03/domain/StudentDetail.java
+++ src/main/java/training/StudentManagement03/domain/StudentDetail.java
@@ -1,13 +1,17 @@
 package training.StudentManagement03.domain;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import training.StudentManagement03.data.Student;
 import training.StudentManagement03.data.StudentCourses;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   private Student student;
