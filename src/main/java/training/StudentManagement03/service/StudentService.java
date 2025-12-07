dcfa0bbd2870c3a9c83127403ecb00794757bb35
Saito-Shogo-1234 <310Gosho@gmail.com>
2025-12-07 12:00:36 +0900

    第31回演習課題 受講状況のデータ追加に伴うServiceのコードの変更とServiceのテスト

--- src/main/java/training/StudentManagement03/service/StudentService.java
+++ src/main/java/training/StudentManagement03/service/StudentService.java
@@ -2,13 +2,16 @@ package training.StudentManagement03.service;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import training.StudentManagement03.controller.converter.StudentConverter;
+import training.StudentManagement03.data.CourseStatus;
 import training.StudentManagement03.data.Student;
 import training.StudentManagement03.data.StudentCourse;
+import training.StudentManagement03.domain.StudentCourseDetail;
 import training.StudentManagement03.domain.StudentDetail;
 import training.StudentManagement03.repository.StudentRepository;
 
@@ -37,25 +40,32 @@ public class StudentService {
   public List<StudentDetail> searchStudentsList() {
     List<Student> studentList = repository.search();
     List<StudentCourse> studentCourseList = repository.searchCourse();
-    return converter.convertStudentDetails(studentList, studentCourseList);
+    List<CourseStatus> statusList = repository.searchStatus();
+    return converter.convertStudentDetails(studentList, studentCourseList, statusList);
   }
 
   /**
    * 受講生詳細検索です。
-   * IDに紐づく受講生情報を取得した後、その受講生に紐づく受講生コース情報を取得して設定します。
+   * IDに紐づく受講生情報を取得した後、その受講生に紐づく受講生コース詳細を取得して設定します。
    *
    * @param id 受講生ID
    * @return 受講生詳細
    */
   public StudentDetail searchStudent(int id) {
     Student student = repository.searchStudent(id);
-    List<StudentCourse> studentCourse = repository.searchStudentCourse(student.getId());
-    return new StudentDetail(student, studentCourse);
+    List<StudentCourse> studentCourseList = repository.searchStudentCourse(student.getId());
+    List<StudentCourseDetail> studentCourseDetailList = new ArrayList<>();
+    for (StudentCourse studentCourse : studentCourseList) {
+      CourseStatus status = repository.searchCourseStatus(studentCourse.getId());
+      StudentCourseDetail studentCourseDetail = new StudentCourseDetail(studentCourse, status);
+      studentCourseDetailList.add(studentCourseDetail);
+    }
+    return new StudentDetail(student, studentCourseDetailList);
   }
 
   /**
    * 受講生詳細の登録を行います。
-   * 受講生と受講生コース情報を個別に登録し、受講生コース情報には受講生詳細を紐づける値とコース開始日コース終了日を設定します。
+   * 受講生と受講生コース詳細を個別に登録し、受講生コース情報には受講生詳細を紐づける値とコース開始日コース終了日を設定します。
    *
    * @param studentDetail 受講生詳細
    * @return 登録情報を付与した受講生詳細
@@ -63,11 +73,16 @@ public class StudentService {
   @Transactional
   public StudentDetail registerStudent(StudentDetail studentDetail) {
     Student student = studentDetail.getStudent();
-    
     repository.insertStudent(student);
-    studentDetail.getStudentsCourseList().forEach(studentCourse -> {
+
+    studentDetail.getStudentCourseDetailList().forEach(studentCourseDetail -> {
+      StudentCourse studentCourse = studentCourseDetail.getStudentCourse();
       initStudentsCourses(studentCourse, student.getId());
       repository.insertStudentCourse(studentCourse);
+
+      CourseStatus status = studentCourseDetail.getCourseStatus();
+      status.setStudentCourseId(studentCourse.getId());
+      repository.insertCourseStatus(status);
     });
     return studentDetail;
   }
@@ -88,14 +103,19 @@ public class StudentService {
 
   /**
    * 受講生詳細の更新を行います。
-   * 受講生と受講生コース情報をそれぞれ更新します。
+   * 受講生と受講生コース詳細をそれぞれ更新します。
    *
    * @param studentDetail 受講生詳細
    */
   @Transactional
   public void updateStudent(StudentDetail studentDetail) {
     repository.updateStudent(studentDetail.getStudent());
-    studentDetail.getStudentsCourseList()
-        .forEach(course -> repository.updateStudentCourse(course));
+    studentDetail.getStudentCourseDetailList().forEach(studentCourseDetail -> {
+          StudentCourse studentCourse = studentCourseDetail.getStudentCourse();
+          repository.updateStudentCourse(studentCourse);
+
+          CourseStatus status = studentCourseDetail.getCourseStatus();
+          repository.updateCourseStatus(status);
+    });
   }
 }
--- src/test/java/training/StudentManagement03/service/StudentServiceTest.java
+++ src/test/java/training/StudentManagement03/service/StudentServiceTest.java
@@ -15,8 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import training.StudentManagement03.controller.converter.StudentConverter;
+import training.StudentManagement03.data.CourseStatus;
 import training.StudentManagement03.data.Student;
 import training.StudentManagement03.data.StudentCourse;
+import training.StudentManagement03.domain.StudentCourseDetail;
 import training.StudentManagement03.domain.StudentDetail;
 import training.StudentManagement03.repository.StudentRepository;
 
@@ -40,45 +42,81 @@ class StudentServiceTest {
   void 受講生詳細の一覧検索＿リポジトリとコンバーターの処理が適切に呼びだせていること() {
     List<Student> studentList = new ArrayList<>();
     List<StudentCourse> studentCourseList = new ArrayList<>();
+    List<CourseStatus> courseStatusList = new ArrayList<>();
+
     when(repository.search()).thenReturn(studentList);
     when(repository.searchCourse()).thenReturn(studentCourseList);
+    when(repository.searchStatus()).thenReturn(courseStatusList);
 
     sut.searchStudentsList();
 
     verify(repository, times(1)).search();
     verify(repository, times(1)).searchCourse();
-    verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
+    verify(repository, times(1)).searchStatus();
+    verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList, courseStatusList);
   }
 
   @Test
   void IDに紐づく受講生詳細の検索＿リポジトリの処理が適切に呼びだせていること() {
     int id = 777;
+
     Student student = new Student();
     student.setId(id);
-    List<StudentCourse> studentCourseList = new ArrayList<>();
+
+    StudentCourse studentCourse1 = new StudentCourse();
+    studentCourse1.setId(100);
+    studentCourse1.setStudentId(student.getId());
+
+    StudentCourse studentCourse2 = new StudentCourse();
+    studentCourse2.setId(200);
+    studentCourse2.setStudentId(student.getId());
+
+    List<StudentCourse> studentCourseList = List.of(studentCourse1, studentCourse2);
+
+    CourseStatus status1 = new CourseStatus();
+    status1.setId(10);
+    status1.setStudentCourseId(studentCourse1.getId());
+
+    CourseStatus status2 = new CourseStatus();
+    status2.setId(20);
+    status2.setStudentCourseId(studentCourse2.getId());
 
     when(repository.searchStudent(id)).thenReturn(student);
     when(repository.searchStudentCourse(student.getId())).thenReturn(studentCourseList);
+    when(repository.searchCourseStatus(studentCourse1.getId())).thenReturn(status1);
+    when(repository.searchCourseStatus(studentCourse2.getId())).thenReturn(status2);
 
     StudentDetail result = sut.searchStudent(id);
 
     verify(repository, times(1)).searchStudent(student.getId());
     verify(repository, times(1)).searchStudentCourse(student.getId());
+    verify(repository, times(1)).searchCourseStatus(studentCourse1.getId());
+    verify(repository, times(1)).searchCourseStatus(studentCourse2.getId());
+
     assertEquals(student, result.getStudent());
-    assertEquals(studentCourseList, result.getStudentsCourseList());
+    assertEquals(studentCourseList.get(0), result.getStudentCourseDetailList().get(0).getStudentCourse());
+    assertEquals(studentCourseList.get(1), result.getStudentCourseDetailList().get(1).getStudentCourse());
+    assertEquals(status1, result.getStudentCourseDetailList().get(0).getCourseStatus());
+    assertEquals(status2, result.getStudentCourseDetailList().get(1).getCourseStatus());
   }
 
   @Test
   void 受講生詳細の登録＿リポジトリの処理が適切に呼びだせていること() {
     Student student = new Student();
+
     StudentCourse studentCourse = new StudentCourse();
-    List<StudentCourse> studentCourseList = List.of(studentCourse);
-    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+    CourseStatus courseStatus = new CourseStatus();
+
+    StudentCourseDetail studentCourseDetail = new StudentCourseDetail(studentCourse, courseStatus);
+    List<StudentCourseDetail> studentCourseDetailList = List.of(studentCourseDetail);
+
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseDetailList);
 
     sut.registerStudent(studentDetail);
 
     verify(repository, times(1)).insertStudent(student);
     verify(repository, times(1)).insertStudentCourse(studentCourse);
+    verify(repository, times(1)).insertCourseStatus(courseStatus);
   }
 
   @Test
@@ -99,13 +137,19 @@ class StudentServiceTest {
   @Test
   void 受講生詳細の更新＿リポジトリの処理が適切に呼びだせていること() {
     Student student = new Student();
+
     StudentCourse studentCourse = new StudentCourse();
-    List<StudentCourse> studentCourseList = List.of(studentCourse);
-    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+    CourseStatus courseStatus = new CourseStatus();
+
+    StudentCourseDetail studentCourseDetail = new StudentCourseDetail(studentCourse, courseStatus);
+    List<StudentCourseDetail> studentCourseDetailList = List.of(studentCourseDetail);
+
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseDetailList);
 
     sut.updateStudent(studentDetail);
 
     verify(repository, times(1)).updateStudent(student);
     verify(repository, times(1)).updateStudentCourse(studentCourse);
+    verify(repository, times(1)).updateCourseStatus(courseStatus);
   }
 }
